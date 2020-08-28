80796134b4c7c07bcbf886f8ec13f3fe0e62d1d9
Mutale85 <45125102+Mutale85@users.noreply.github.com>
2020-08-28 02:26:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Your Site</title>
 	<meta charset="utf-8">
-	<script src="https://domain.com/js/script.js?userdata=userID"></script>
+	<script src="https://domain.com/script.js?userdata=userID"></script>
 </head>
 <body>
   <h1>Testing</h1>
