99296a8a3c93068e5aac82ea2946657cac2c309f
Mutale85 <45125102+Mutale85@users.noreply.github.com>
2020-08-28 02:15:41 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+!DOCTYPE html>
+<html>
+<head>
+	<title>Your Site</title>
+	<meta charset="utf-8">
+	<script src="https://domain.com/js/script.js?userdata=userID"></script>
+</head>
+<body>
+  <h1>Testing</h1>
+</body>
+</html>
